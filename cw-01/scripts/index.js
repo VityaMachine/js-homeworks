34bbe6e98d58251129c152cd6067a94e3d449913
Vitya Machine <vatamanenko9@gmail.com>
2022-12-14 13:33:14 +0200

    cw01 and hw01 added

--- cw-01/scripts/index.js
+++ cw-01/scripts/index.js
@@ -2,6 +2,23 @@ var userName = prompt("Введіть ім'я");
 var userSurname = prompt("Введіть прізвище");
 var userAge = prompt("Введіть свій вік");
 
+var numbers = [];
+
+while (numbers.length < 3) {
+  var inputedValue = prompt(`Введіть значення числа ${numbers.length + 1}`);
+
+  if (!isNaN(parseFloat(inputedValue))) {
+    numbers.push(parseFloat(inputedValue));
+  } else alert("невірний формат даних");
+}
+
+var numbersSum = numbers.reduce(
+  function(sum, elem) {
+    return sum + elem
+  }, 0
+) 
+var avgNumber = numbersSum / numbers.length;
+
 var header = `<hearer class="header">Header</hearer>`;
 var nav = `<nav class="nav">Nav</nav>`;
 var section = `<section class="section">
@@ -17,6 +34,9 @@ var section = `<section class="section">
                     <div>
                       Вік: ${userAge}
                     </div>
+                    <div>
+                      Середнє значення введених чисел: ${avgNumber}
+                    </div>                    
                 </div>
               </section>`;
 
