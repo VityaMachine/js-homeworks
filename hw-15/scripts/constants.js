437c940150de5e123e09fb7c6bd75f0504d0c40d
Vitya Machine <vatamanenko9@gmail.com>
2023-03-12 00:18:46 +0200

    add notFoundConstant

--- hw-15/scripts/constants.js
+++ hw-15/scripts/constants.js
@@ -1,5 +1,6 @@
 const PAGE_LOGIN = 'login';
 const PAGE_REGISTER = 'register'
+const PAGE_NOT_FOUND = 'not_found'
 
 
-export { PAGE_LOGIN, PAGE_REGISTER};
+export { PAGE_LOGIN, PAGE_REGISTER, PAGE_NOT_FOUND};
